102b4f071edab0d67d1aa02948875c07c64917da
Jhonatas Siqueira <jhonatas131997@hotmail.com>
2024-04-02 22:47:59 -0300

    Update config return message

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const { SearchLinkOrderService, SearchInvoiceServices, SearchAccountReceive, SearchInformationClient } = require('./omie')
-const { SendMessageNewInvoice } = require('./index')
 const { MessageText } = require('./digisac')
 
 const app = express();
@@ -13,10 +12,29 @@ app.use(express.json());
 
 app.route('/v1/webhooks/new-service-invoice')
     .get((req, res) => res.json(''))
-    .post((req, res) => {
-        const sendMessage = SendMessageNewInvoice(req.body.event.idOrdemServico, req.body.event.idCliente)
+    .post(async(req, res) => {
+
+        if(req.body !== ''){
+            var idOrderService = await req.body.event.idOrdemServico
+            var codClient = req.body.event.idCliente
+            var client = await SearchInformationClient(codClient)
+            var nameClient = client.razao_social
+            var linkOrderService = await SearchLinkOrderService(idOrderService)
+            var linkOrderService = linkOrderService.cLinkPortal
+            var invoice = await SearchInvoiceServices(idOrderService)
+            var numberInvoice = invoice.Cabecalho.nNumeroNFSe
+            var valueInvoice = invoice.Cabecalho.nValorNFSe.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+            var account = await SearchAccountReceive(idOrderService)
+            var dueDate = account.data_vencimento
+            var numberTelefone = "+55" + client.telefone1_ddd + client.telefone1_numero
         
-        return res.json('')
+            var message = `
+                Olá ${nameClient}!\n\nA nota fiscal ${numberInvoice} com vencimento em ${dueDate} no valor de ${valueInvoice} está disponível!\n\n*Clique no link para acessar a Nota Fiscal/Boleto*\n${linkOrderService}`
+        
+            MessageText(numberTelefone, message)
+        
+            return res.json({message: req.body})
+        }
     })
 
 
