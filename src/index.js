4e1d747d0eb7c772e0acc806f9f23eb6446f91aa
Jhonatas Siqueira <jhonatas131997@hotmail.com>
2024-04-02 21:05:46 -0300

    Update config return message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT ?? 3000
 
 app.use(express.json());
 
-app.use('/webhook/new-service-invoice', async (req, res) => {
+app.post('/webhook/new-service-invoice', async (req, res) => {
     var idOrderService = req.body.event.idOrdemServico
     var codClient = req.body.event.idCliente
     var client = await SearchInformationClient(codClient)
@@ -23,18 +23,13 @@ app.use('/webhook/new-service-invoice', async (req, res) => {
     var dueDate = account.data_vencimento
     var numberTelefone = "+55" + client.telefone1_ddd + client.telefone1_numero
 
-
+    
     var message = `
         Olá ${nameClient}!\n\nA nota fiscal ${numberInvoice} com vencimento em ${dueDate} no valor de ${valueInvoice} está disponível!\n\n*Clique no link para acessar a Nota Fiscal/Boleto*\n${linkOrderService}`
 
     MessageText(numberTelefone, message)
 
-    res.send('Servidor logado!...')
     res.status(200).json(req.body)
-})
-
-app.use('/', (req, res) => {
-    return res.send('Servidor logado!')
 })
 
 app.listen(port, () => console.log('Server is running on port ', port))
