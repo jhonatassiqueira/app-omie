9da4962df94b3f201614c46f475fc7a9fb1ea4f3
Jhonatas Siqueira <jhonatas131997@hotmail.com>
2024-04-02 21:09:14 -0300

    Update config return message

--- src/index.js
+++ src/index.js
@@ -23,12 +23,13 @@ app.post('/webhook/new-service-invoice', async (req, res) => {
     var dueDate = account.data_vencimento
     var numberTelefone = "+55" + client.telefone1_ddd + client.telefone1_numero
 
-    
+
     var message = `
         Olá ${nameClient}!\n\nA nota fiscal ${numberInvoice} com vencimento em ${dueDate} no valor de ${valueInvoice} está disponível!\n\n*Clique no link para acessar a Nota Fiscal/Boleto*\n${linkOrderService}`
 
     MessageText(numberTelefone, message)
 
+    res.send("Integration success!")
     res.status(200).json(req.body)
 })
 
