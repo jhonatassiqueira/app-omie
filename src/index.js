5961e73e4e2c16a3b0aa01c23089269263fd94b0
Jhonatas Siqueira <jhonatas131997@hotmail.com>
2024-04-02 21:30:33 -0300

    Update config return message

--- src/index.js
+++ src/index.js
@@ -9,27 +9,32 @@ const port = process.env.PORT ?? 3000
 
 app.use(express.json());
 
-app.use('/webhook/new-service-invoice', async (req, res) => {
-    var idOrderService = await req.body.event.idOrdemServico
-    var codClient = req.body.event.idCliente
-    var client = await SearchInformationClient(codClient)
-    var nameClient = client.razao_social
-    var linkOrderService = await SearchLinkOrderService(idOrderService)
-    var linkOrderService = linkOrderService.cLinkPortal
-    var invoice = await SearchInvoiceServices(idOrderService)
-    var numberInvoice = invoice.Cabecalho.nNumeroNFSe
-    var valueInvoice = invoice.Cabecalho.nValorNFSe.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
-    var account = await SearchAccountReceive(idOrderService)
-    var dueDate = account.data_vencimento
-    var numberTelefone = "+55" + client.telefone1_ddd + client.telefone1_numero
-
-
-    var message = `
-        Olá ${nameClient}!\n\nA nota fiscal ${numberInvoice} com vencimento em ${dueDate} no valor de ${valueInvoice} está disponível!\n\n*Clique no link para acessar a Nota Fiscal/Boleto*\n${linkOrderService}`
-
-    MessageText(numberTelefone, message)
-
-    res.status(200).json(req.body)
+app.use('/v1/webhooks/new-service-invoice', async (req, res) => {
+    
+    if(req.method === 'GET'){
+        return res.json('')
+    }else{
+        var idOrderService = await req.body.event.idOrdemServico
+        var codClient = req.body.event.idCliente
+        var client = await SearchInformationClient(codClient)
+        var nameClient = client.razao_social
+        var linkOrderService = await SearchLinkOrderService(idOrderService)
+        var linkOrderService = linkOrderService.cLinkPortal
+        var invoice = await SearchInvoiceServices(idOrderService)
+        var numberInvoice = invoice.Cabecalho.nNumeroNFSe
+        var valueInvoice = invoice.Cabecalho.nValorNFSe.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})
+        var account = await SearchAccountReceive(idOrderService)
+        var dueDate = account.data_vencimento
+        var numberTelefone = "+55" + client.telefone1_ddd + client.telefone1_numero
+    
+    
+        var message = `
+            Olá ${nameClient}!\n\nA nota fiscal ${numberInvoice} com vencimento em ${dueDate} no valor de ${valueInvoice} está disponível!\n\n*Clique no link para acessar a Nota Fiscal/Boleto*\n${linkOrderService}`
+    
+        MessageText(numberTelefone, message)
+    
+        res.status(200).json(req.body)
+    }
 })
 
 app.listen(port, () => console.log('Server is running on port ', port))
